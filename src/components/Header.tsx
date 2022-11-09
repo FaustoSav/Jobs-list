37d06fc31a5dadab92dd3c5c4122cbba7a15e175
Fausto Savoya <84111811+FaustoSav@users.noreply.github.com>
2022-11-09 18:34:38 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import image from './Images'
 export const Header = () => {
   return (
     <header className='h-30 w-full max-h-30 h-32 bg-mobile sm:bg-desktop bg-desaturedCyan '>
-      {/* <img src={image[10].src} alt="header-img" className='  w-full max-h-32' /> */}
+      
     </header>
   )
 }
